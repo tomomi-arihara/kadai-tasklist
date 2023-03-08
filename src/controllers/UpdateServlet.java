51d9a70768d2ff65670c20ec3bd26d2d97fc64ae
tomomi.arihara <shakotaro@gmail.com>
2023-03-08 23:59:20 +0900

    存在しないidに対する処理を追加

--- src/controllers/UpdateServlet.java
+++ src/controllers/UpdateServlet.java
@@ -37,8 +37,8 @@ public class UpdateServlet extends HttpServlet {
         if(_token != null && _token.equals(request.getSession().getId())) {
             EntityManager em = DBUtil.createEntityManager();
 
-            // セッションスコープからメッセージのIDを取得して
-            // 該当のIDのメッセージ1件のみをデータベースから取得
+            // セッションスコープからタスクのIDを取得して
+            // 該当のIDのタスク1件のみをデータベースから取得
             Tasks m = em.find(Tasks.class, (Integer)(request.getSession().getAttribute("tasks_id")));
 
             // フォームの内容を各フィールドに上書き
